9cb7bb725b35c82e97322ee13271b1b75ce9dc62
Sergio <sergiosear@gmail.com>
2018-01-15 11:34:00 +0100

    Version 2.0

---
+++ src/tarea/Tarea2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea;
+
+/**
+ *
+ * @author virtualmint17
+ */
+public class Tarea2 {
+    int copa = 90;
+}
